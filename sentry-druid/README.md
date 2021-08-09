6b386aa1c0b5e4f9493de111db2a2e96dd4c4641
Arpad Borsos <arpad.borsos@sentry.io>
2021-08-09 11:38:49 +0200

    add a textbox example to druid

--- sentry-druid/README.md
+++ sentry-druid/README.md
@@ -4,4 +4,5 @@
 
 # Notes
 
-- The `TextBox` Widget can’t handle Neo2 Layer3 Keys on mac (haven’t tested other OSs yet)
+- The `TextBox` Widget can’t handle Neo2 Layer3 Keys on mac (haven’t tested other OSs yet).
+- Comes with localizable widgets based on fluent.
--- sentry-druid/src/main.rs
+++ sentry-druid/src/main.rs
@@ -1,20 +1,33 @@
-use druid::widget::{Button, Flex, Label};
-use druid::{AppLauncher, LocalizedString, PlatformError, Widget, WidgetExt, WindowDesc};
+use druid::widget::{Button, Flex, Label, TextBox};
+use druid::{
+    AppLauncher, Data, Lens, LocalizedString, PlatformError, Widget, WidgetExt, WindowDesc,
+};
 
 fn main() -> Result<(), PlatformError> {
     let main_window = WindowDesc::new(ui_builder);
-    let data = 0_u32;
-    AppLauncher::with_window(main_window).launch(data)
+    AppLauncher::with_window(main_window).launch(HelloState::default())
 }
 
-fn ui_builder() -> impl Widget<u32> {
+#[derive(Clone, Default, Data, Lens)]
+struct HelloState {
+    counter: u32,
+    name: String,
+}
+
+fn ui_builder() -> impl Widget<HelloState> {
     // The label text will be computed dynamically based on the current locale and count
-    let text =
-        LocalizedString::new("hello-counter").with_arg("count", |data: &u32, _env| (*data).into());
+    let text = LocalizedString::new("hello-counter")
+        .with_arg("count", |data: &HelloState, _env| (*data).counter.into());
     let label = Label::new(text).padding(5.0).center();
     let button = Button::new("increment")
-        .on_click(|_ctx, data, _env| *data += 1)
+        .on_click(|_ctx, data: &mut HelloState, _env| data.counter += 1)
         .padding(5.0);
+    let textbox = TextBox::new()
+        .with_placeholder("some placeholder")
+        .lens(HelloState::name);
 
-    Flex::column().with_child(label).with_child(button)
+    Flex::column()
+        .with_child(label)
+        .with_child(button)
+        .with_child(textbox)
 }
