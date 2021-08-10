af5032839752dee22e08bb6e1bb4fafe87c3768c
Arpad Borsos <arpad.borsos@sentry.io>
2021-08-10 14:16:15 +0200

    thread titles out to the main rust code

--- sentry-sixty/src/main.rs
+++ sentry-sixty/src/main.rs
@@ -3,6 +3,8 @@ sixtyfps::include_modules!();
 fn main() {
     let main_window = MainWindow::new();
 
+    main_window.set_header_title("oh hai".into());
+    main_window.set_header_subtitle("subtitle!".into());
     main_window.on_close_clicked(sixtyfps::quit_event_loop);
 
     main_window.run();
