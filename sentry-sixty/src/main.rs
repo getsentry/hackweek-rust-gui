11085057e39180fb09ea71d13d04eeee6d110f2d
Sebastian Zivota <loewenheim@mailbox.org>
2021-08-12 12:41:19 +0200

    Separate main windows for submitting envelopes & feedback

--- sentry-sixty/src/main.rs
+++ sentry-sixty/src/main.rs
@@ -1,9 +1,14 @@
 use std::sync::Arc;
+use std::sync::Mutex;
 
 use i18n_embed::fluent::{fluent_language_loader, FluentLanguageLoader};
 use i18n_embed::DesktopLanguageRequester;
 use i18n_embed_fl::fl;
 use rust_embed::RustEmbed;
+use sentry_core::protocol::{EnvelopeItem, Event};
+use sentry_core::types::Uuid;
+use sentry_core::Envelope;
+use sentry_uploader::{Uploader, UserFeedback};
 
 #[derive(RustEmbed)]
 #[folder = "../resources/l18n"] // path to the compiled localization resources
@@ -13,11 +18,19 @@ sixtyfps::include_modules!();
 
 pub mod config;
 
-fn make_mock_envelope() -> sentry_core::Envelope {
-    let mut event = sentry_core::protocol::Event::new();
+type RawEnvelope = Vec<u8>;
+
+#[derive(Debug, Clone)]
+enum UiState {
+    SubmitEnvelope(RawEnvelope),
+    SubmitFeedback(Uuid),
+}
+
+fn make_mock_envelope() -> Envelope {
+    let mut event = Event::new();
     event.message = Some(String::from("Wow, something bad happened"));
-    let mut envelope: sentry_core::Envelope = event.into();
-    envelope.add_item(sentry_core::protocol::EnvelopeItem::Attachment(
+    let mut envelope: Envelope = event.into();
+    envelope.add_item(EnvelopeItem::Attachment(
         sentry_core::protocol::Attachment {
             buffer: include_bytes!("main.rs").to_vec(),
             filename: String::from("main.rs"),
@@ -31,8 +44,6 @@ fn make_mock_envelope() -> sentry_core::Envelope {
 // TODO: make it possible to use this as a library, and call the functions
 // starting the UI from crashpad/your own app/some kind of wrapper.
 pub fn start_crash_reporter_windows(config: config::Config) {
-    let main_window = MainWindow::new();
-
     let dsn = config.sentry.dsn.parse().unwrap();
 
     let uploader_config = sentry_uploader::Config::new(
@@ -40,51 +51,48 @@ pub fn start_crash_reporter_windows(config: config::Config) {
         config.sentry.org_slug.clone(),
         config.sentry.project_slug.clone(),
     );
-    let uploader = Arc::new(sentry_uploader::Uploader::new(uploader_config));
-
+    let uploader = Arc::new(Uploader::new(uploader_config));
     let envelope = Arc::new(make_mock_envelope());
 
     // we move one ref into the submit callback (for now, possibly more callbacks soon)
     let uploader = Arc::clone(&uploader);
     let envelope = Arc::clone(&envelope);
 
+    let mut raw_envelope = Vec::new();
+    envelope.to_writer(&mut raw_envelope).unwrap();
+
+    let ui_state = Arc::new(Mutex::new(UiState::SubmitEnvelope(raw_envelope)));
+
+    let main_window = MainWindow::new();
+    let main_window_weak = main_window.as_weak();
     localize(&main_window, &config.branding);
+
     main_window.on_close_clicked(sixtyfps::quit_event_loop);
 
     main_window.on_submit_clicked(move || {
-        let rt = tokio::runtime::Builder::new_current_thread()
-            .enable_all()
-            .build()
-            .unwrap();
-
-        // make borrow checker happy for now.
-        let envelope = envelope.as_ref();
+        // make borrow checker happy
         let uploader = uploader.as_ref();
+        let mut ui_state = ui_state.lock().unwrap();
+        match *ui_state {
+            UiState::SubmitEnvelope(ref mut raw_envelope) => {
+                let raw_envelope = std::mem::take(raw_envelope);
+                let event_id = submit_envelope(&uploader, raw_envelope).unwrap();
+                *ui_state = UiState::SubmitFeedback(event_id);
+                main_window_weak.unwrap().set_step(2);
+            }
+            UiState::SubmitFeedback(event_id) => {
+                let mut user_feedback = sentry_uploader::UserFeedback::default();
+                // TODO: take the values from the UI, and only send user feedback if all
+                // the required fields are provided
+                user_feedback.name = String::from("Bender");
+                user_feedback.email = String::from("bender@planetexpress.earth");
+                user_feedback.comment = String::from("oh noes! its a me, user feedbackaaaa!");
 
-        rt.block_on(async move {
-            let mut raw_envelope = Vec::new();
-            envelope.to_writer(&mut raw_envelope).unwrap();
+                submit_feedback(&uploader, event_id, &user_feedback).unwrap();
 
-            let event_id = uploader.send_envelope(raw_envelope).await?;
-
-            let mut user_feedback = sentry_uploader::UserFeedback::default();
-            // TODO: take the values from the UI, and only send user feedback if all
-            // the required fields are provided
-            user_feedback.name = String::from("Bender");
-            user_feedback.email = String::from("bender@planetexpress.earth");
-            user_feedback.comment = String::from("oh noes! its a me, user feedbackaaaa!");
-
-            uploader
-                .send_user_feedback(event_id, &user_feedback)
-                .await?;
-
-            Ok::<_, Box<dyn std::error::Error>>(())
-        })
-        // TODO: surface any kind of errors up to the UI. though the only action
-        // in that case would be to say uploading failed, and the user will just
-        // close the app.
-        .unwrap();
-        sixtyfps::quit_event_loop();
+                sixtyfps::quit_event_loop()
+            }
+        }
     });
 
     main_window.run();
@@ -135,4 +143,27 @@ fn localize(main_window: &MainWindow, branding: &config::Branding) {
     main_window.set_footer_show_text(fl!(loader, "app-footer-toggle-show-files").into());
     main_window.set_footer_close_text(fl!(loader, "app-footer-close").into());
     main_window.set_footer_submit_text(fl!(loader, "app-footer-submit").into());
+}
+
+fn submit_envelope(uploader: &Uploader, raw_envelope: Vec<u8>) -> anyhow::Result<Uuid> {
+    let rt = tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+
+    rt.block_on(async move { uploader.send_envelope(raw_envelope).await })
+}
+
+fn submit_feedback(
+    uploader: &Uploader,
+    event_id: Uuid,
+    feedback: &UserFeedback,
+) -> anyhow::Result<()> {
+    let rt = tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+
+    rt.block_on(async move { uploader.send_user_feedback(event_id, feedback).await })?;
+    Ok(())
 }
